ebe148b8c4d386b6c79396bfb8e249ec841e56fd
hirock0 <hirockdutta0@gmail.com>
2025-05-08 21:50:15 +0600

    dashboard retouch-22

--- src/app/(routes)/employee/dashboard/page.tsx
+++ src/app/(routes)/employee/dashboard/page.tsx
@@ -2,16 +2,22 @@ export const dynamic = "force-dynamic";
 import { AllJobs } from "@/app/actions/apis/Apis";
 import ApplicationCard from "@/components/(dashboards)/applicationCard/ApplicationCard";
 import Link from "next/link";
+import { FindAllApplications } from "@/app/actions/apis/Apis";
 const DashboardPage = async () => {
   const reqAllJobs = await AllJobs();
   const allJobs = await reqAllJobs?.allJobs;
+  const reqApplications = await FindAllApplications();
+  const allApplications = await reqApplications?.applications;
   const approvedJobs = await allJobs?.filter(
     (item: any) => item?.approvedStatus === true
+  );
+  const approvedApplications = await allApplications?.filter(
+    (item: any) => item?.status === "approved"
   );
 
   return (
     <div className=" flex flex-col gap-5">
-      <div className=" grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 justify-items-center gap-5">
+      <div className=" grid grid-cols-2 max-md:grid-cols-1 justify-items-center gap-5">
         <Link
           href={`/employee/posted/jobs`}
           className=" w-full tooltip tooltip-bottom"
@@ -26,6 +32,7 @@ const DashboardPage = async () => {
             title2="Jobs"
           />
         </Link>
+
         <Link
           href={`/employee/approved/jobs`}
           className=" w-full tooltip tooltip-bottom"
@@ -40,8 +47,10 @@ const DashboardPage = async () => {
             title2="Jobs"
           />
         </Link>
+      </div>
+      <div className=" grid grid-cols-2 max-md:grid-cols-1 justify-items-center gap-5">
         <Link
-          href={`/employee/rejected/jobs`}
+          href={`/employee/posted/applications`}
           className=" w-full tooltip tooltip-bottom"
           data-tip="Click"
         >
@@ -49,9 +58,24 @@ const DashboardPage = async () => {
             design={
               " cursor-pointer text-center flex-col gap-2 bg-gradient-to-tl from-yellow-300 via-yellow-300 text-2xl to-red-400 w-full   h-52 rounded-xl p-5 flex items-center justify-center shadow-xl lg:hover:scale-105"
             }
-            applications={approvedJobs}
-            title="Rejected"
-            title2="Jobs"
+            applications={allApplications}
+            title="User Posted"
+            title2="Applications"
+          />
+        </Link>
+
+        <Link
+          href={`/employee/approved/applications`}
+          className=" w-full tooltip tooltip-bottom"
+          data-tip="Click"
+        >
+          <ApplicationCard
+            design={
+              " cursor-pointer text-center flex-col gap-2 bg-gradient-to-tl from-green-300 via-yellow-300 text-2xl to-yellow-400 w-full   h-52 rounded-xl p-5 flex items-center justify-center shadow-xl lg:hover:scale-105"
+            }
+            applications={approvedApplications}
+            title="Approved"
+            title2="Applications"
           />
         </Link>
       </div>
--- src/app/(routes)/user/dashboard/page.tsx
+++ src/app/(routes)/user/dashboard/page.tsx
@@ -11,7 +11,7 @@ const DashboardPage = async () => {
 
   return (
     <div className=" flex flex-col gap-5">
-      <div className=" grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 justify-items-center gap-5">
+      <div className=" grid grid-cols-2 max-md:grid-cols-1 justify-items-center gap-5">
         <Link
           href={`/user/applied/applications`}
           className=" w-full tooltip tooltip-bottom"
@@ -37,20 +37,6 @@ const DashboardPage = async () => {
             }
             applications={applications}
             title="Approved"
-            title2="Applications"
-          />
-        </Link>
-        <Link
-          href={`/user/rejected/applications`}
-          className=" w-full tooltip tooltip-bottom"
-          data-tip="Click"
-        >
-          <ApplicationCard
-            design={
-              " cursor-pointer text-center flex-col gap-2 bg-gradient-to-tl from-yellow-300 via-yellow-300 text-2xl to-red-400 w-full   h-52 rounded-xl p-5 flex items-center justify-center shadow-xl lg:hover:scale-105"
-            }
-            applications={applications}
-            title="Rejected"
             title2="Applications"
           />
         </Link>
