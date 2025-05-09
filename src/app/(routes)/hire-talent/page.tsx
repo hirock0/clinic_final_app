63cf1853e5494e4bb912bc6d106526293e7692ec
rubayat-rafi <rubayatrafi522@gmail.com>
2025-05-09 22:52:22 +0600

    clinical support staffing page create

--- src/app/(routes)/hire-talent/page.tsx
+++ src/app/(routes)/hire-talent/page.tsx
@@ -99,7 +99,7 @@ const HireTalentPage = () => {
     dispatch(fetchData());
   }, [dispatch]);
   return (
-    <div className="max-w-4xl mx-auto p-6 rounded-lg shadow-md my-12 md:my-20">
+    <section className="scroll-mt-40 max-w-4xl mx-auto p-6 rounded-lg shadow-md my-12 md:my-20">
       <h1 className="text-2xl font-bold text-gray-800 mb-2">
         Healthcare Facility Staffing Request Form
       </h1>
@@ -446,7 +446,7 @@ const HireTalentPage = () => {
           </button>
         </div>
       </form>
-    </div>
+    </section>
   );
 };
 
