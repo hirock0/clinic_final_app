a3d1b9057295e5243ea86238fab156ac31baa3ee
hirock0 <hirockdutta0@gmail.com>
2025-05-13 03:35:23 +0600

    eslint recorrect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,8 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-expressions": "off",
-      "react/no-unescaped-entities":"off"
+      "react/no-unescaped-entities":"off",
+      "react-hooks/exhaustive-deps":"off"
     },
   },
 ];
