993fc1a99cf408a43905a2e5051ce00d3847edb0
rubayat-rafi <rubayatrafi522@gmail.com>
2025-05-14 16:05:46 +0600

    changes photo

--- src/components/(home)/strategic_advisors/Strategic_Advisors.tsx
+++ src/components/(home)/strategic_advisors/Strategic_Advisors.tsx
@@ -8,22 +8,22 @@ const Strategic_Advisors = () => {
 
   const strategicAdvisors = [
     {
-      name: "Emily Wilson",
-      title: "Chief Medical Officer",
-      photo: "/team/bussiness-man.png",
-      bio: "Board-certified physician leading clinical quality and compliance initiatives."
+      name: "Roy Rodriguez",
+      title: "STRATEGIC ADVISOR -I",
+      photo: "/team/roy.jpg",
+      bio: "Brings unmatched expertise in healthcare business, backed by 30+ years of experience driving growth and operational excellence."
     },
     {
-      name: "Sarah Johnson",
-      title: "Chief Operations Officer",
+      name: "Dr. Abu Nasar",
+      title: "STRATEGIC ADVISOR -II",
       photo: "/team/Picture-04.jpg",
-      bio: "Specializes in operational efficiency and staff development for healthcare providers."
+      bio: "With over 30 years of healthcare experience, guides our growth with expert insight, integrity, and a focus on quality care."
     }
 
   ]
 
   const handleActiveCard = (index: number) => {
-    setActiveIndex(prev =>(prev === index ? null : index));
+    setActiveIndex(prev => (prev === index ? null : index));
   }
 
 
@@ -35,8 +35,8 @@ const Strategic_Advisors = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 w-full lg:w-1/2">
           {strategicAdvisors.map((member, index) => (
             <div key={index}
-            onClick={() => handleActiveCard(index)}
-            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow relative group">
+              onClick={() => handleActiveCard(index)}
+              className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow relative group">
               <div className="relative aspect-square">
                 <Image
                   src={member.photo}
@@ -45,12 +45,12 @@ const Strategic_Advisors = () => {
                   className="object-cover items-start justify-start"
                 />
               </div>
-              <div className={`absolute bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-[#308d89] to-transparent transition-transform group-hover:translate-y-0  duration-300 ease-in-out
+              <div className={`absolute bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-[#ffdb61] to-transparent transition-transform group-hover:translate-y-0  duration-300 ease-in-out
                 ${activeIndex === index ? 'translate-y-0' : 'translate-y-full'}
                 `}>
-                <h3 className="text-xl font-semibold second-text-color text-shadow-[#1e1e1e] text-shadow-xs">{member.name}</h3>
-                <p className="second-text-color font-medium">{member.title}</p>
-                <p className="mt-3 text-gray-700">{member.bio}</p>
+                <h3 className="text-lg font-semibold second-text-color text-shadow-[#1e1e1e] text-shadow-xs">{member.name}</h3>
+                <p className="second-text-color font-medium test-sm">{member.title}</p>
+                <p className="mt-2 text-gray-700 text-xs">{member.bio}</p>
               </div>
             </div>
           ))}
